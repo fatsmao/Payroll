fef6f70818f22bfd7093214339fa60c5d236d29a
fatsmao <maofatma@gmail.com>
2022-02-26 15:27:17 +0000

    Changed status code to 201

--- src/main/java/com/example/springboot/EmployeesController.java
+++ src/main/java/com/example/springboot/EmployeesController.java
@@ -1,9 +1,7 @@
 package com.example.springboot;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -22,6 +20,7 @@ public class EmployeesController {
     }
 
     @PostMapping("/AddEmployee")
+    @ResponseStatus( HttpStatus.CREATED )
     public void addEmployee(@RequestBody Employee employee) {
        employeeRepositoryField.addEmployee(employee);
 
