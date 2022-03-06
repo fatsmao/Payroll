f9fd21e769464cf6ca950f7d94e9cf22ddfdbbc8
fatsmao <maofatma@gmail.com>
2022-03-06 15:53:04 +0000

    Added Admin requests to Controller class

--- src/main/java/com/example/demo/Controller/PayrollController.java
+++ src/main/java/com/example/demo/Controller/PayrollController.java
@@ -1,6 +1,8 @@
 package com.example.demo.Controller;
 
+import com.example.demo.POJO.Admin;
 import com.example.demo.POJO.Employee;
+import com.example.demo.Service.AdminService;
 import com.example.demo.Service.EmployeeService;
 import org.springframework.web.bind.annotation.*;
 
@@ -9,11 +11,11 @@ import java.util.List;
 @RestController
 public class PayrollController {
     private final EmployeeService employeeService;
-//    private AdminService userService;
+    private final AdminService adminService;
 //    private MonthService monthService;
 
-    public PayrollController(EmployeeService employeeService) {
-//        this.userService = userService;
+    public PayrollController(AdminService adminService, EmployeeService employeeService) {
+        this.adminService = adminService;
         this.employeeService = employeeService;
 //        this.monthService = monthService;
     }
@@ -24,7 +26,7 @@ public class PayrollController {
         return employeeService.displayEmployee();
     }
 
-    @PostMapping("/Employee")
+    @PostMapping("/Employees")
     public void addEmployee(@RequestBody Employee employee){
         employeeService.addEmployee(employee);
     }
@@ -38,4 +40,26 @@ public class PayrollController {
     public void updateEmployee(@PathVariable("id") Integer id, @RequestBody Employee updatedEmployee){
         employeeService.updateEmployee(updatedEmployee);
     }
+
+    //Admin
+    @GetMapping("/Admin")
+    public List<Admin> getAdmin(){
+        return adminService.displayAdmin();
+    }
+
+    @PostMapping("/Admin")
+    public void addAdmin(@RequestBody Admin admin){
+        adminService.addAdmin(admin);
+    }
+
+    @DeleteMapping("/Admin")
+    public void deleteAdmin(@PathVariable("id") Integer id){
+        adminService.deleteAdmin(id);
+    }
+
+    @PutMapping("/Admin/{id}")
+    public void updateAdmin(@RequestBody Admin admin){
+        adminService.updateAdmin(admin);
+    }
+
 }
--- src/main/java/com/example/demo/Service/AdminService.java
+++ src/main/java/com/example/demo/Service/AdminService.java
@@ -1,10 +1,98 @@
 package com.example.demo.Service;
 
+import com.example.demo.DAO.AdminDAO;
 import com.example.demo.POJO.Admin;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service
 public class AdminService {
+
+    private final AdminDAO adminDAO;
+
+    public AdminService(AdminDAO adminDAO) {
+        this.adminDAO = adminDAO;
+    }
+
+    private boolean adminExists(Integer id) {
+        return adminDAO
+                .listAdmin()
+                .stream()
+                .anyMatch(user -> user.getID().equals(id));
+    }
+
+    private boolean isSecurePassword(String password) {
+
+        boolean hasLetter = false;
+        boolean hasDigit = false;
+
+        if (password.length() >= 8) {
+            for (int i = 0; i < password.length(); i++) {
+                char x = password.charAt(i);
+                if (Character.isLetter(x)) {
+                    hasLetter = true;
+                } else if (Character.isDigit(x)) {
+                    hasDigit = true;
+                }
+
+                if (hasLetter && hasDigit) {
+                    return true;
+                }
+            }
+            if (hasLetter && hasDigit) {
+            } else {
+                throw new IllegalStateException("Your password must include numbers and letters");
+            }
+        } else {
+            throw new IllegalStateException("Your password must have at least 8 characters");
+        }
+        return false;
+    }
+
+    public void addAdmin(Admin admin) {
+        boolean exists = adminExists(admin.getID());
+        boolean secure = isSecurePassword(admin.getPassword());
+        if (exists) {
+            throw new IllegalStateException("Admin user already exists");
+        } else if (admin.getName() == null) {
+            throw new IllegalStateException("No name entered.");
+        } else if (admin.getUsername() == null) {
+            throw new IllegalStateException("No username entered");
+        } else if (admin.getPassword() == null) {
+            throw new IllegalStateException("No password entered");
+        } else if (!secure) {
+            throw new IllegalStateException("Password not secure");
+        } else if (admin.getIsAdmin()) {
+            throw new IllegalStateException("Manager already exists.");
+        } else {
+            adminDAO.addAdmin(admin);
+        }
+    }
+
+    public void deleteAdmin(Integer Id) {
+        boolean exists = adminExists(Id);
+        if (!exists) {
+            throw new IllegalStateException("Admin user does not exist");
+        } else {
+            adminDAO.deleteAdmin(Id);
+        }
+    }
+
+    public void updateAdmin(Admin admin) {
+        boolean exists = adminExists(admin.getID());
+
+        int result = adminDAO.updateAdmin(admin);
+
+        if (result != 1) { //if result isn't one then you know that something failed.
+            throw new IllegalStateException("Could not update user in database. Input not valid");
+        }
+    }
+
+    public List<Admin> displayAdmin() {
+        return adminDAO.listAdmin();
+    }
+
 }
 
 //    int addAdmin (Admin admin);
